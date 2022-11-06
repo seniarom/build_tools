c3483aad5410213ec471d1df3e573dbf74f23237
Kseniia <seniarom@gmail.com>
2022-11-06 22:05:07 +0300

    Lesson 3

---
+++ Lesson3/src/items.js
@@ -0,0 +1,60 @@
+import image1 from "../src/media/images/1.jpg"
+import image2 from "../src/media/images/2.jpg"
+import image3 from "../src/media/images/3.jpg"
+import image4 from "../src/media/images/4.gif"
+
+import audio1 from "../src/media/audio/1.mp3"
+import audio2 from "../src/media/audio/2.mp3"
+import audio3 from "../src/media/audio/3.mp3"
+
+import video1 from "../src/media/video/1.mp4"
+import video2 from "../src/media/video/2.mp4"
+
+export default [
+    {
+        resource: image1,
+        type: "image",
+        title: "Мельница у реки"
+    },
+    {
+        resource: image2,
+        type: "image",
+        title: "Горы осенью"
+    },
+    {
+        resource: image3,
+        type: "image",
+        title: "Деревня летом"
+    },
+    {
+        resource: image4,
+        type: "image",
+        title: "Анимированный котик"
+    },
+    {
+        resource: audio1,
+        type: "audio",
+        title: "Поют птицы"
+    },
+    {
+        resource: audio2,
+        type: "audio",
+        title: "Ручей в ущелье"
+    },
+    {
+        resource: audio3,
+        type: "audio",
+        title: "Закат в лесу"
+    },
+    {
+        resource: video1,
+        type: "video",
+        title: "Never Gonna Give You Up"
+    },
+    {
+        resource: video2,
+        type: "video",
+        title: "Морской прибой"
+    }
+]
+
